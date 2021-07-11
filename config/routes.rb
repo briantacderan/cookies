e3d1014c24289f7ef60607ad31d56ab812d3c2c7
Brian Tacderan <briantacderan@gmail.com>
2021-07-11 12:22:46 -0700

    Revert "Revert payment API changes"
    This reverts commit 7b37873a23e4dbc7fc9f2ed30b807cbc75aa0c86

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
     
-  #devise_for :users
+  devise_for :users
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'menu' => 'pages#menu'
-  #resources :orders
+  resources :orders
     
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 2021_07_09_143644) do
 
   create_table "chips", force: :cascade do |t|
@@ -32,7 +30,6 @@ ActiveRecord::Schema.define(version: 2021_07_09_143644) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-=begin
   create_table "orders", force: :cascade do |t|
     t.integer "product_id"
     t.integer "user_id"
@@ -66,6 +63,5 @@ ActiveRecord::Schema.define(version: 2021_07_09_143644) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-=end
 
 end
