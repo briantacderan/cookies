b69efb928da36e72a13f3c50ee995702592a90eb
Brian Tacderan <briantacderan@gmail.com>
2021-07-22 17:45:04 -0700

    Revert changes that were discarded in last commit

--- app/controllers/stripe/checkouts_controller.rb
+++ app/controllers/stripe/checkouts_controller.rb
@@ -1,55 +1,49 @@
 class Stripe::CheckoutsController < ApplicationController
   before_action :get_cart_total
+  before_action :authenticate_user!
   
   def new
     add_cookie_to_cart(params) if params
-    authenticate_user!
     flash[:success] = "Cart updated"
     redirect_to root_path
   end
 
   def show
-    @line_items ||= session[:line_items]
-    authenticate_user!
   end
 
   def create
     if current_user.stripe_id?
       customer = Stripe::Customer.retrieve(current_user.stripe_id)
     else
-      if current_user
-        customer = StripeTool.create_customer(
-          email: current_user.email, 
-          stripe_token: params[:stripeToken]
-        )
-        current_user.update!(stripe_id: customer.id)
-      else
-        redirect_to new_user_session_path
-      end
+      customer = StripeTool.create_customer(
+        email: current_user.email
+      )
     end
     
+    current_user.update!(stripe_id: customer.id)
     stripe_session = Stripe::Checkout::Session.create({
       payment_method_types: ['card'],
-      line_items: @line_items,
+      line_items: session[:line_items],
       customer: customer.id,
       client_reference_id: current_user.id,
       mode: 'payment',
       success_url: "#{stripe_thanks_url}?session_id={CHECKOUT_SESSION_ID}",
       cancel_url: root_url
     })
-    
+      
     redirect_to stripe_session.url
+  rescue Stripe::InvalidRequestError => e
+    flash[:error] = 'You silly goose! Your cart is empty!'
+    redirect_to stripe_cart_path
   end
  
   def thanks
     session.delete(:line_items)
-    @line_items = Array.new
     flash[:success] = "Purchase successful"
   end
 
   def destroy
     session.delete(:line_items)
-    @line_items = Array.new
     flash[:success] = "Cart deleted"
     redirect_to stripe_cart_path
   end
@@ -78,7 +72,7 @@ class Stripe::CheckoutsController < ApplicationController
   private
     
   def add_cookie_to_cart(params)
-    price = params[:item][:menu_id] == 1 ? 150 : 200
+    price = params[:item][:menu_id].to_i == 1 ? 150 : 200
     line_item = {
       price_data: {
         unit_amount: price,
@@ -90,19 +84,20 @@ class Stripe::CheckoutsController < ApplicationController
       },
       quantity: params[:item][:quantity]
     }
-    @line_items.push(line_item)
-    session[:line_items] = @line_items
+    session[:line_items].push(line_item)
   end
 
   def get_cart_total
     @amount = 0
-    @line_items ||= Array.new
-    unless @line_items.nil?
-      @line_items.each do |i| 
-        @amount += i[:price_data][:unit_amount] * i[:quantity]
+    if session[:line_items]
+      session[:line_items].each do |i|
+        quantity = i['quantity'].to_i
+        price = i['price_data']['unit_amount'].to_i
+        @amount += price * quantity
       end
+    else
+      session[:line_items] = Array.new
     end
-    return @amount
   end
 
   def build_subscription(stripe_subscription)
